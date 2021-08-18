77bf95a0de444c6422778a18403b841ff16f1988
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-18 08:56:01 -0700

    refactored for output, Fetch womens stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -14,18 +14,21 @@ Whistler = Store.create(name: "Whistler", annual_revenue: 1900000 , mens_apparel
 Yaletown = Store.create(name: "Yaletown ", annual_revenue: 430000, mens_apparel:true, womens_apparel:true)
 
 @mens_stores = Store.where(mens_apparel:true)
+@womens_stores = Store.where(womens_apparel:true, annual_revenue: 0..999999)
 
-
-
-@mens_stores.each do |store|
-  name = store[:name]
-  annual_revenue = store[:annual_revenue]
-
-  puts "#{name} has an anual income of #{annual_revenue}"
+def output_stores (stores)
+  stores.each do |store|
+    name = store[:name]
+    annual_revenue = store[:annual_revenue]
+  
+    puts "#{name} has an anual income of #{annual_revenue}"
+  end
 end
 
-@womens_stores = Store.where(womens_apparel:true, annual_revenue < 1000000)
-# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+output_stores(@mens_stores)
+output_stores(@womens_stores)
+
+
 
 
 # Exercise 4: Loading a subset of stores
