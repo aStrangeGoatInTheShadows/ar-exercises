4acfa0610ce5394c426efba6833e76858b4b0eae
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-18 09:15:01 -0700

    provided sums and average income

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "The total sum of revenues is $#{Store.sum(:annual_revenue)}"
+puts "The average of revenue is $#{Store.sum(:annual_revenue) / Store.count}"
+
+large_stores = Store.where(annual_revenue: 1000000..Float::INFINITY)
+
+puts "The ammount of stores making over $1M is #{large_stores.count}"
